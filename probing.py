82f28dcc28a79375924143cae9d248c566a91f64
Roy David <roydavid957@gmail.com>
2023-05-17 13:51:10 +0200

    bug fix

--- probing.py
+++ probing.py
@@ -96,6 +96,7 @@ def main():
     
     args = parser.parse_args()
     args.device = "cuda" if torch.cuda.is_available() else "cpu"
+    args.key = 'event_embedding' if args.use_event_embeddings else 'embedding'
 
     print('\n\nUsing sentence Embeddings:',args.use_sentence_embeddings)
     print('\nUsing event Embeddings:', args.use_event_embeddings)
@@ -105,10 +106,6 @@ def main():
     train_samples, labels_list = load_all_samples(args.train_file, args)                                             # Loading of train and test samples from
     valid_samples, test_label_list = load_all_samples(args.test_file, args) if args.test_file else [],labels_list    # train and test source files
     all_labels = list(set(test_label_list).union(set(labels_list)))
-    args.key = 'event_embedding' if args.use_event_embeddings else 'embedding'
-    max_len_dev = max([len(np.concatenate(x.representation[1][f"input_{args.key}"]+[x.representation[1][f"target_{args.key}"]])) for x in train_samples])                           # get the max length
-    max_len_test = max([len(np.concatenate(x.representation[1][f"input_{args.key}"]+[x.representation[1][f"target_{args.key}"]])) for x in valid_samples]) if valid_samples else 0  # for padding the (CMCNC) inputs 
-    args.max_len = max_len_dev if max_len_dev >= max_len_test else max_len_test                                                                                                     # for the SVM inputs
 
     model_config = AutoConfig.from_pretrained(args.model_ckpt)    
     args.num_hidden_layers = model_config.num_hidden_layers
@@ -116,6 +113,10 @@ def main():
     extract_samples_representations(train_samples, args)    # Computation of samples representation using contextual embeddings
     if valid_samples:
        extract_samples_representations(valid_samples, args)
+
+    max_len_dev = max([len(np.concatenate(x.representation[1][f"input_{args.key}"]+[x.representation[1][f"target_{args.key}"]])) for x in train_samples])                           # get the max length
+    max_len_test = max([len(np.concatenate(x.representation[1][f"input_{args.key}"]+[x.representation[1][f"target_{args.key}"]])) for x in valid_samples]) if valid_samples else 0  # for padding the (CMCNC) inputs 
+    args.max_len = max_len_dev if max_len_dev >= max_len_test else max_len_test                                                                                                     # for the SVM inputs
 
     all_labels += ['macro avg', 'weighted avg', 'accuracy']
     
